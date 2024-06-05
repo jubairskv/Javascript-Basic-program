5b93c543cd595e3d9f49ffcd0f01df18a0ff47ac
jubairskv <jubairskv@gmail.com>
2024-06-05 20:49:50 +0530

    Basic Program

---
+++ index.js
@@ -0,0 +1,16 @@
+//Reverse an array
+let arr =[1,2,3,4,5,6]
+let rev=arr.reverse()
+console.log(rev)
+
+//reverse the string
+let str="jubair"
+let revr=str.split("").reverse().join("")
+console.log(revr)
+
+
+//sort the array
+let srt =[1,2,7,8,9,3,5,4]
+let sort=srt.sort()
+console.log(sort)
+
