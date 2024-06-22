bce57698f9ad8a101a53aff78862e73f53ecf999
jubairskv <jubairskv@gmail.com>
2024-06-22 21:29:21 +0530

    pattern program

--- arrayProgram.js
+++ arrayProgram.js
@@ -174,16 +174,16 @@ function pattern(n){
 
   function pattern4(n){
     for(let i=0;i<n;i++){
-      for(let j=0;j<=i;j++){
+      for(let j=0;j<i;j++){
         process.stdout.write(" ")
       }
-      for(let j=0;j<2*n-2*i+1;j++){
+      for(let j=0;j<2*n-(2*i+1);j++){
         process.stdout.write("*")
       }
-       for(let j=0;j<=i;j++){
+       for(let j=0;j<i;j++){
         process.stdout.write(" ")
       }
       console.log()
     }
   }
-  patern4(6)
+  pattern4(6)
