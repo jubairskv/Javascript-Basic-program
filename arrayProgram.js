00113742a7c06dc9d4159a6f01a9a1507aaf3fb3
jubairskv <jubairskv@gmail.com>
2024-06-24 13:10:58 +0530

    pattern program alphabet

--- arrayProgram.js
+++ arrayProgram.js
@@ -102,176 +102,201 @@
 
 
 //pattern program
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    process.stdout.write("* ");
-  }
-  console.log();
-}
+// for (let i = 0; i < 4; i++) {
+//   for (let j = 0; j < 4; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
 
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j <= i; j++) {
-    process.stdout.write("* ")
-  }
-  console.log()
-}
-
-
-for (let i = 1; i <= 5; i++) {
-  for (j = 1; j <= i; j++) {
-    process.stdout.write(j.toString())
-  }
-  console.log()
-}
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     process.stdout.write("* ")
+//   }
+//   console.log()
+// }
 
 
-for (let i = 1; i <= 5; i++) {
-  for (j = 1; j <= i; j++) {
-    process.stdout.write(i.toString())
-  }
-  console.log()
-}
-
-function pattern(n) {
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < n - i + 1; j++) {
-      process.stdout.write("*")
-    }
-    console.log()
-  }
-}
-pattern(6)
-
-function pattern2() {
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < n - i + 1; j++) {
-      process.stdout.write(j.toString())
-    }
-    console.log()
-  }
-}
-pattern2(6)
+// for (let i = 1; i <= 5; i++) {
+//   for (j = 1; j <= i; j++) {
+//     process.stdout.write(j.toString())
+//   }
+//   console.log()
+// }
 
 
+// for (let i = 1; i <= 5; i++) {
+//   for (j = 1; j <= i; j++) {
+//     process.stdout.write(i.toString())
+//   }
+//   console.log()
+// }
 
-function patern3(n) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      process.stdout.write(" ")
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-      process.stdout.write("*")
-    }
-    for (let j = 0; j < n - i - 1; j++) {
-      process.stdout.write(" ")
-    }
-    console.log()
-  }
-}
+// function pattern(n) {
+//   for (let i = 1; i < n; i++) {
+//     for (let j = 1; j < n - i + 1; j++) {
+//       process.stdout.write("*")
+//     }
+//     console.log()
+//   }
+// }
+// pattern(6)
 
-patern3(6)
-
-
-function pattern4(n) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < i; j++) {
-      process.stdout.write(" ")
-    }
-    for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
-      process.stdout.write("*")
-    }
-    for (let j = 0; j < i; j++) {
-      process.stdout.write(" ")
-    }
-    console.log()
-  }
-}
-pattern4(6)
+// function pattern2() {
+//   for (let i = 1; i < n; i++) {
+//     for (let j = 1; j < n - i + 1; j++) {
+//       process.stdout.write(j.toString())
+//     }
+//     console.log()
+//   }
+// }
+// pattern2(6)
 
 
-function pattern5(n) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      process.stdout.write(" ")
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-      process.stdout.write("*")
-    }
-    for (let j = 0; j < n - i - 1; j++) {
-      process.stdout.write(" ")
-    }
-    console.log()
-  }
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < i; j++) {
-      process.stdout.write(" ")
-    }
-    for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
-      process.stdout.write("*")
-    }
-    for (let j = 0; j < i; j++) {
-      process.stdout.write(" ")
-    }
-    console.log()
-  }
-}
-pattern5(6)
 
-function pattern7(n) {
-  for (let i = 1; i <=2 * n + 1; i++) {
-    let stars = i;
-    if (i > n) stars = 2 * n - i;
-    for(let j = 1 ;j<=stars;j++){
-      process.stdout.write("*")
-    }
-    console.log()
-  }
-}
-pattern7(6)
+// function patern3(n) {
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       process.stdout.write(" ")
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//       process.stdout.write("*")
+//     }
+//     for (let j = 0; j < n - i - 1; j++) {
+//       process.stdout.write(" ")
+//     }
+//     console.log()
+//   }
+// }
+
+// patern3(6)
 
 
-function pattern8(n){
-  let start =1;
-  for(let i=0;i<n;i++){
-    if(i%2==0) start =1;
-    else start =0
-    for(let j=0;j<=i;j++){
-      process.stdout.write(start)
-      start=1-start
-    }
-    console.log()
-  }
-}
-pattern8(6)
+// function pattern4(n) {
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < i; j++) {
+//       process.stdout.write(" ")
+//     }
+//     for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
+//       process.stdout.write("*")
+//     }
+//     for (let j = 0; j < i; j++) {
+//       process.stdout.write(" ")
+//     }
+//     console.log()
+//   }
+// }
+// pattern4(6)
 
 
-function pattern9(n){
-  let space =2*(n-1);
-  for(let i=0;i<=n;i++){
-    for(let j=0;j<=i;j++){
-      process.stdout.write(j.toString())
-    }
-    for(let j=1;j<=space;j++){
-      process.stdout.write(" ")
-    }
-    for(let j=i;j>=1;j--){
-      process.stdout.write(j.toString())
-    }
-    console.log()
-    space-=2
-  }
-}
-pattern9(6)
+// function pattern5(n) {
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       process.stdout.write(" ")
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//       process.stdout.write("*")
+//     }
+//     for (let j = 0; j < n - i - 1; j++) {
+//       process.stdout.write(" ")
+//     }
+//     console.log()
+//   }
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < i; j++) {
+//       process.stdout.write(" ")
+//     }
+//     for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
+//       process.stdout.write("*")
+//     }
+//     for (let j = 0; j < i; j++) {
+//       process.stdout.write(" ")
+//     }
+//     console.log()
+//   }
+// }
+// pattern5(6)
 
-function pattern10(n){
-  let num =1
-  for(let i=1;i<=n;i++){
-    for(let j=1;j<=i;j++){
-      process.stdout.write(num.toString()+ " ")
-      num=num+1
-    }
-    console.log()
-  }
+// function pattern7(n) {
+//   for (let i = 1; i <=2 * n + 1; i++) {
+//     let stars = i;
+//     if (i > n) stars = 2 * n - i;
+//     for(let j = 1 ;j<=stars;j++){
+//       process.stdout.write("*")
+//     }
+//     console.log()
+//   }
+// }
+// pattern7(6)
+
+
+// function pattern8(n){
+//   let start =1;
+//   for(let i=0;i<n;i++){
+//     if(i%2==0) start =1;
+//     else start =0
+//     for(let j=0;j<=i;j++){
+//       process.stdout.write(start)
+//       start=1-start
+//     }
+//     console.log()
+//   }
+// }
+// pattern8(6)
+
+
+// function pattern9(n){
+//   let space =2*(n-1);
+//   for(let i=0;i<=n;i++){
+//     for(let j=0;j<=i;j++){
+//       process.stdout.write(j.toString())
+//     }
+//     for(let j=1;j<=space;j++){
+//       process.stdout.write(" ")
+//     }
+//     for(let j=i;j>=1;j--){
+//       process.stdout.write(j.toString())
+//     }
+//     console.log()
+//     space-=2
+//   }
+// }
+// pattern9(6)
+
+// function pattern10(n){
+//   let num =1
+//   for(let i=1;i<=n;i++){
+//     for(let j=1;j<=i;j++){
+//       process.stdout.write(num.toString()+ " ")
+//       num=num+1
+//     }
+//     console.log()
+//   }
   
+// }
+// pattern10(5)
+
+
+// function pattern12(n){
+//   for(let i =0;i<n;i++){
+//     let alpha=""
+//     for(let j =0;j<=(n-i-1);j++){
+//       alpha+=String.fromCharCode(65+j)+" "
+//     }
+//     console.log(alpha)
+//   }
+// }
+// pattern12(5)
+
+function pattern13(n) {
+  for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j <=n-i-1 ; j++) {
+      row += String.fromCharCode(65+j)+" "
+    }
+    console.log(row);
+  }
 }
-pattern10(5)
+
+pattern13(6);
+
