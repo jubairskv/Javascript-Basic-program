fbfe333834ecb700721c8608b3aaf14aa739b4d4
jubairskv <jubairskv@gmail.com>
2024-07-23 00:04:59 +0530

    DSA Algorithm

--- Basic math program.js
+++ Basic math program.js
@@ -1,60 +1,49 @@
 //Count DIgit -burte force Apporch
-function countDigit(n){
-    let count=0
-    while(n>0){
-       // lastDigit=n%10;
-        n=Math.floor(n/10)
-        count=count+1                   //Time complexity-0(log10(n))
-       
-    }
-    return count
+function CountDigits(n) {
+  let count = 0;
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+    //count=count+1
+  }
+  return count;
 }
-let ans = countDigit(1562212)
+
+const ans = CountDigits(12145)
 console.log(ans)
 
-
 //CountDigit optimal approch
-function Count(n){
-    return log=Math.floor(Math.log10(n)+1);       
+function Count(n) {
+  return (log = Math.floor(Math.log10(n) + 1));
 }
-let ans1=Count(1234567)
-console.log(ans1)
-
+let ans1 = Count(1234567);
+console.log(ans1);
 
 //Reverse a number:
-function Reverse(n){
-    ReverseNumber=0
-    while(n>0){
-        lastDigit=n%10
-        ReverseNumber=(ReverseNumber*10)+lastDigit
-        n=Math.floor(n/10)
-       
-    }
-    console.log(ReverseNumber)        //return reversNumber
+function Reverse(n) {
+  ReverseNumber = 0;
+  while (n > 0) {
+    lastDigit = n % 10;
+    ReverseNumber = ReverseNumber * 10 + lastDigit;
+    n = Math.floor(n / 10);
+  }
+  console.log(ReverseNumber); //return reversNumber
 }
-Reverse(122897)
-
-
-
+Reverse(122897);
 
 //Check palindrom:
-function Reverse(n){
-   let ReverseNumber=0
-let  dup=n;
-    while(n>0){
-        lastDigit=n%10
-        ReverseNumber=(ReverseNumber*10)+lastDigit
-        n=Math.floor(n/10)
-       
-    }
-    if(dup===ReverseNumber){
-       return true
-    }else{
-       return false
-    }              // console.log(ReverseNumber)        //return reversNumber
+function Reverse(n) {
+  let ReverseNumber = 0;
+  let dup = n;
+  while (n > 0) {
+    lastDigit = n % 10;
+    ReverseNumber = ReverseNumber * 10 + lastDigit;
+    n = Math.floor(n / 10);
+  }
+  if (dup === ReverseNumber) {
+    return true;
+  } else {
+    return false;
+  } // console.log(ReverseNumber)        //return reversNumber
 }
-console.log(Reverse(121))
-
-
-
-
+console.log(Reverse(121));
