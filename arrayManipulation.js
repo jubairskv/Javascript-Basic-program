0a9cf1bf11613a9ada3a14cbcf8105d59a032eaa
jubairskv <jubairskv@gmail.com>
2024-07-27 14:24:46 +0530

    array manipulation

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,7 +9,7 @@ let arr = [1, 2, 3, 4]; //[0,1,2,3]
 let newElement = 0;
 for (let i = arr.length; i > 0; i--) {
   console.log("before:", arr[i]);
-  arr[i] = arr[i - 1]; //arr[4] = arr[4-1]   //arr[4] =arr[3]  //array number
+  arr[i] = arr[i - 1];                 //arr[4] = arr[4-1]   //arr[4] =arr[3]  //array number
   console.log("after:", arr[i]);
 }
 arr[0] = newElement;
