856188cf27ac84dc9960c06fa15904bd91ea5d0a
jubairskv <jubairskv@gmail.com>
2024-07-27 14:22:22 +0530

    array manipulation

--- loopIteraation.js
+++ loopIteraation.js
@@ -1,4 +1,4 @@
-// How loop is working
+// // How loop is working
 for (let i = 0; i <= 4; i++) {
   console.log(i);
 }
@@ -13,4 +13,14 @@ for (let i = 0; i < 4; i++) {
 
 for (let i = 4; i > 0; i--) {
   console.log(i);
+}
+
+let arr = [1, 2, 3, 4, 5];
+//index based loop:
+for (let x in arr) {
+  console.log("index" ,x);
+}
+//value bassed loop:
+for (let x of arr) {
+  console.log("value" ,x);
 }
