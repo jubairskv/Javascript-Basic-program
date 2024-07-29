fbd4a08a107c9aeb03b8f9970015a995806fea5f
jubairskv <jubairskv@gmail.com>
2024-07-29 20:32:18 +0530

    Array Easy Problem

--- arrayProgram(EASY).js
+++ arrayProgram(EASY).js
@@ -1,7 +1,8 @@
 //Find largest number in an array:
 let arr = [2, 5, 8, 3, 4, 5, 1];
 let largest = arr[0];
-for (let i = 0; i < arr.length; i++) {        //TimeComplexity = o(n)
+for (let i = 0; i < arr.length; i++) {
+  //TimeComplexity = o(n)
   if (arr[i] > largest) {
     largest = arr[i];
   }
@@ -12,10 +13,12 @@ console.log(largest);
 //find smallest number in an array:
 let arr1 = [2, 5, 8, 3, 4, 5, 1];
 let smallest = arr[0];
-for (let i = 0; i < arr1.length; i++) {      //TimeComplexity = o(n)
+for (let i = 0; i < arr1.length; i++) {
+  //TimeComplexity = o(n)
   if (arr[i] < smallest) {
     smallest = arr[i];
   }
   //return smallest
 }
 console.log(smallest);
+
