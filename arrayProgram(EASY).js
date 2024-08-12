56718871893169420a0ab3af48ff53d78b82ff2c
jubairskv <jubairskv@gmail.com>
2024-08-12 23:14:39 +0530

    practice code daily

--- arrayProgram(EASY).js
+++ arrayProgram(EASY).js
@@ -107,27 +107,66 @@
 // }
 
 //find missing and repeating number in an array:  [brutu force approach]
-function findMissingandrepesting(arr) {
-  let repeating = -1;
-  let missing = -1;
-  for (let i = 1; i <= arr.length; i++) {
-    let count = 0;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        count++;
-      }
-    }
-    if (count === 2) {
-      repeating = i;
-    } else if (count === 0) {
-      missing = i;
-    }
-    if (repeating != -1 && missing != -1) {
-      break;
-    }
+// function findMissingandrepesting(arr) {
+//   let repeating = -1;
+//   let missing = -1;
+//   for (let i = 1; i <= arr.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] === i) {
+//         count++;
+//       }
+//     }
+//     if (count === 2) {
+//       repeating = i;
+//     } else if (count === 0) {
+//       missing = i;
+//     }
+//     if (repeating != -1 && missing != -1) {
+//       break;
+//     }
+//   }
+//   return [repeating, missing];
+// }
+// arr = [1, 2, 4, 5, 6, 6, 6, 7, 7];
+// const ans = findMissingandrepesting(arr);
+// console.log(ans[0], ans[1]);
+
+
+
+function findMissingRepeatingNumbers(a) {
+  const n = a.length; // size of the array
+
+  // Find Sn and S2n:
+  const SN = (n * (n + 1)) / 2;
+  const S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+  // Calculate S and S2:
+  let S = 0, S2 = 0;
+  for (let i = 0; i < n; i++) {
+      S += a[i];
+      S2 += a[i] * a[i];
   }
-  return [repeating, missing];
+
+  //S-Sn = X-Y:
+  const val1 = S - SN;
+
+  // S2-S2n = X^2-Y^2:
+  let val2 = S2 - S2N;
+
+  //Find X+Y = (X^2-Y^2)/(X-Y):
+  val2 = val2 / val1;
+
+  //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+  // Here, X-Y = val1 and X+Y = val2:
+  const x = (val1 + val2) / 2;
+  const y = x - val1;
+
+  return [x, y];
 }
-arr = [1, 2, 4, 5, 6, 6, 6, 7, 7];
-const ans = findMissingandrepesting(arr);
-console.log(ans[0], ans[1]);
+
+const a = [3, 1, 2, 5, 4, 6, 7, 5];
+const ans = findMissingRepeatingNumbers(a);
+console.log(`The repeating and missing numbers are: [${ans[0]}, ${ans[1]}]`);
+
+
